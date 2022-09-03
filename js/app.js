c60c5b0cb36ace2361c89c3e85ae4c06491d97e8
TahmidTaki <tahmid.taki@live.iium.edu.my>
2022-09-03 17:16:09 +0800

    news according to category loaded

--- js/app.js
+++ js/app.js
@@ -21,8 +21,25 @@ const displayCategory = (data) => {
         const categoryLink = document.createElement('div');
         // categoryLink.innerHTML = `${category.category_name}`;
         categoryLink.innerHTML = `
-        <a class="text-decoration-none text-secondary" href="">${category.category_name}</a>
-        `
+        <a class="text-decoration-none text-secondary" onclick="loadCategoryNews('${category.category_id}')" >${category.category_name}</a>
+        `;
         categoryList.appendChild(categoryLink);
     })
 }
+
+
+
+//load news specific to category
+const loadCategoryNews = async (categoryId) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`;
+    console.log(url);
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+// loadCategoryNews('03');
